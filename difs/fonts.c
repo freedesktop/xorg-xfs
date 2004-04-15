993c7446a8c253df9e437b9c301dc25fe4fdea56
Egbert Eich <eich@suse.de>
2004-04-15 10:17:42 +0000

    Merged changes from RELEASE-1 branch

--- difs/fonts.c
+++ difs/fonts.c
@@ -46,7 +46,6 @@ in this Software without prior written authorization from The Open Group.
  * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
  * THIS SOFTWARE.
  */
-/* $XFree86: xc/programs/xfs/difs/fonts.c,v 3.13 2001/12/14 20:01:34 dawes Exp $ */
 /*#define DEBUG*/
 #include        "FS.h"
 #include        "FSproto.h"
